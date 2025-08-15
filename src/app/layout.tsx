172874394647f542114458bb0f38afee7b3fa403
tejupanghal <todaynewsindia7@gmail.com>
2025-08-15 22:47:37 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -65,7 +65,7 @@ export default function RootLayout({
 				<meta name="google-site-verification" content="YOUR-VERIFICATION-CODE" />
 
 				{/* Google AdSense */}
-				<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-XXXXXXXXXXXXXXXX" crossOrigin="anonymous"></script>
+				<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4500547980522610" crossOrigin="anonymous"></script>
 
 				{/* Google Analytics */}
 				<GoogleAnalytics />
